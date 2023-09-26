3f7522aa4c06790989fa33838ef78ab449e6c976
rommelsanthiago <rommel.santhiago@gmail.com>
2023-09-26 14:06:18 -0300

    My improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,257 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 1279;
+const limit = 12;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
+  let soma = 0;
+  for (let key in pokemon.baseStats) {
+    soma += pokemon.baseStats[key];
+  }
+
+  let gender = "";
+
+  if (pokemon.gender.genderless) {
+    gender = "genderless";
+  } else {
+    gender = `<i class="fa fa-mars" style="font-size:24px;color:#1e90ff"></i>${pokemon.gender.male}
+    <i class="fa fa-venus" style="font-size:24px;color:#ff1493"></i>${pokemon.gender.female}`;
+  }
+
+  const li = document.createElement("li");
+  li.classList.add("pokemon", pokemon.type);
+  li.setAttribute("dataset", pokemon.number);
+  li.innerHTML = `
+        <div class="wallpaper ${pokemon.type}">
+          <div>
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
+          </div>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+          <div class="detail">
+            <ol class="types">
+              ${pokemon.types
+                .map((type) => `<li class="type ${type}">${type}</li>`)
+                .join("")}
+            </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+          </div>
+        </div>
+        `;
+  li.addEventListener("click", () => {
+    const modal = document.createElement("div");
+    modal.classList.add("modalContainer", `poke${pokemon.number}`, `visible`);
+    modal.innerHTML = `
+          <div class="modal pokemonDetail">
+          <div class="pokeSpecie ${pokemon.type}">
+          <button class="closeModal">X</button>
+          <div class="pokemon">
+            <div class="identification">
+            <span class="number">#${pokemon.number}</span>
+              <span class="name">${pokemon.name}</span>
             </div>
-        </li>
-    `
-}
+            <div class="detail">
+              <ol class="types">
+              ${pokemon.types
+                .map((type) => `<li class="type ${type}">${type}</li>`)
+                .join("")}
+              </ol>
+              <div class="pokeContainer">
+                <img src="${pokemon.photo2}" alt="${pokemon.name}" />
+              </div>
+            </div>
+          </div>
+          <div class="menu">
+            <ul>
+              <li class="active" data-target="aboutTable">
+                <a href="#">About</a>
+              </li>
+              <li data-target="baseStatsTable">
+                <a href="#">Base Stats</a>
+              </li>
+            </ul>
+          </div>
+          <div id="aboutTable" class="section">
+            <table>
+              <tr>
+                <td>Habitat</td>
+                <td>${pokemon.habitat}</td>
+              </tr>
+              <tr>
+                <td>Height</td>
+                <td>${pokemon.height}</td>
+              </tr>
+              <tr>
+                <td>Weight</td>
+                <td>${pokemon.weight}</td>
+              </tr>
+              <tr>
+                <td>Abilities</td>
+                <td>${pokemon.abilities.slice().join(", ")}</td>
+              </tr>
+              <th>Breeding</th>
+              <tr>
+                <td>Gender</td>
+                <td>
+                  ${gender}
+                </td>
+              </tr>
+              <tr>
+                <td>Egg Groups</td>
+                <td>${pokemon.eggGroups.slice().join(", ")}</td>
+              </tr>
+              <tr>
+                <td>Egg Cycle</td>
+                <td>${pokemon.eggCycle} steps</td>
+              </tr>
+            </table>
+          </div>
+          <div id="baseStatsTable" class="section">
+            <table>
+              <tr>
+                <td>HP</td>
+                <td>${pokemon.baseStats.hp}</td>
+                <td>
+                  <progress max="255" value="${
+                    pokemon.baseStats.hp
+                  }"></progress>
+                </td>
+              </tr>
+              <tr>
+                <td>Attack</td>
+                <td>${pokemon.baseStats.attack}</td>
+                <td>
+                  <progress max="255" value="${
+                    pokemon.baseStats.attack
+                  }"></progress>
+                </td>
+              </tr>
+              <tr>
+                <td>Defense</td>
+                <td>${pokemon.baseStats.defense}</td>
+                <td>
+                  <progress max="255" value="${
+                    pokemon.baseStats.defense
+                  }"></progress>
+                </td>
+              </tr>
+              <tr>
+                <td>Sp. Atk</td>
+                <td>${pokemon.baseStats.specialAttack}</td>
+                <td>
+                  <progress max="255" value="${
+                    pokemon.baseStats.specialAttack
+                  }"></progress>
+                </td>
+              </tr>
+              <tr>
+                <td>Sp. Def</td>
+                <td>${pokemon.baseStats.specialDefense}</td>
+                <td>
+                  <progress max="255" value="${
+                    pokemon.baseStats.specialDefense
+                  }"></progress>
+                </td>
+              </tr>
+              <tr>
+                <td>Speed</td>
+                <td>${pokemon.baseStats.speed}</td>
+                <td>
+                  <progress max="255" value="${
+                    pokemon.baseStats.speed
+                  }"></progress>
+                </td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>${soma}</td>
+                <td>
+                  <progress max="600" value="${soma}"></progress>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+          `;
+    pokemonList.appendChild(modal);
+    const closeButton = modal.querySelector(".closeModal");
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
-}
+    closeButton.addEventListener("click", () => {
+      modal.remove();
+    });
 
-loadPokemonItens(offset, limit)
+    function toggleActive(e) {
+      const opt = document.querySelectorAll(".menu li");
+      
+      opt.forEach((link) => {
+        e.preventDefault();
+        link.classList.remove("active");
+      });
+      e.currentTarget.classList.add("active");
 
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+      const target = e.currentTarget.dataset.target;
+      const sections = document.querySelectorAll(".section");
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
-
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
+      sections.forEach((section) => {
+        if (section.id === target) {
+          section.style.display = "block";
+        } else {
+          section.style.display = "none";
+        }
+      });
     }
-})
+
+    const opt = document.querySelectorAll(".menu li");
+    opt.forEach((link) => {
+      link.addEventListener("click", toggleActive);
+    });
+  });
+  return li;
+}
+
+loadMoreButton.classList.add("loader")
+
+function loadPokemonItems(offset, limit, loading = true) {
+  setTimeout(() => {
+    pokeApi
+    .getPokemons(offset, limit)
+    .then((pokemons = []) => {
+      const newHtml = pokemons.map(convertPokemonToLi);
+      newHtml.forEach(LiElement =>{
+        pokemonList.appendChild(LiElement)
+      })
+    })
+    .catch((error) => {
+      const sectionContent = document.querySelector(".content");
+      const p = document.createElement("p");
+      p.innerText =
+        "Parece que estamos com um problema, verifique sua internet e tente recarregar a página, caso o erro persista, entre em contato pelo Linkedin";
+      sectionContent.appendChild(p);
+    });
+  }, 2000);
+}
+
+setTimeout(() => loadMoreButton.classList.remove("loader"), 2500);
+
+loadPokemonItems(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  loadMoreButton.classList.add("loader")
+  offset += limit;
+  const qtdRecordsWithNexPage = offset + limit;
+
+  if (qtdRecordsWithNexPage >= maxRecords) {
+    setTimeout(() => loadMoreButton.classList.remove("loader"), 3000);
+    const newLimit = maxRecords - offset;
+    loadPokemonItems(offset, newLimit)
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItems(offset, limit);
+    setTimeout(() => loadMoreButton.classList.remove("loader"), 3000);
+  }
+});
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -5,4 +5,20 @@ class Pokemon {
     type;
     types = [];
     photo;
+    photo2;
+    habitat;
+    height;
+    weight;
+    abilities = [];
+    gender = {};
+    eggGroups = [];
+    eggCycle;
+    baseStats = {
+      hp: 0,
+      attack: 0,
+      defense: 0,
+      specialAttack: 0,
+      specialDefense: 0,
+      speed: 0
+    };
 }
